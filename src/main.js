aae23a81e0c1fbf5119e6e25eb9f4decd618c300
julijabelka <julija.belka@gmail.com>
2024-04-05 00:21:48 +0200

    end home wrk

--- src/main.js
+++ src/main.js
@@ -13,10 +13,52 @@ const loader = document.querySelector('.loader');
 
 form.addEventListener('submit', handleSubmit);
 
+const lightbox = new SimpleLightbox('.gallery-link', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  overlay: true,
+  overlayOpacity: 0.7,
+});
+
 function handleSubmit(event) {
   event.preventDefault();
+
+  loaderStart();
+  gallery.innerHTML = '';
   const questForm = event.target['queryInput'].value;
-  getPictures(questForm).then(
-    response => (gallery.innerHTML = createMarkup(response.hits))
-  );
+
+  if (questForm !== '') {
+    getPictures(questForm)
+      .then(response => {
+        if (response.hits.length === 0) {
+          return iziToast.error({
+            message:
+              'Sorry, there are no images matching your search query. Please try again!',
+            position: 'topRight',
+          });
+        }
+        gallery.innerHTML = createMarkup(response.hits);
+
+        lightbox.refresh();
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => {
+        loaderStop();
+      });
+  } else {
+    loaderStop();
+    iziToast.error({
+      message: 'Please, enter a word',
+      position: 'topRight',
+    });
+  }
+}
+
+function loaderStart() {
+  loader.classList.remove('is-hidden');
+}
+function loaderStop() {
+  loader.classList.add('is-hidden');
 }
